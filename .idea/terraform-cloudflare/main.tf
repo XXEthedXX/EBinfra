0c19b1816b7fe37027fc4b5fba56fc49971912f4
Access Point <ebalikalaba@my.stlcc.edu>
2025-06-18 14:38:55 -0500

    resource / dns record

--- .idea/terraform-cloudflare/main.tf
+++ .idea/terraform-cloudflare/main.tf
@@ -7,7 +7,26 @@ terraform {
   }
 }
 
-provider "cloudflare" {
-  # Configuration optionz
+provider "cloudflare" { # Configuration options
+  api_token = var.cloudflare_api_token
+}
 
+variable "zone_id" {
+  default = "7464bce49e4134da1b7a148a027d0a13"
+}
+
+variable "account_id" {
+  default = "e9fe21cb5a614d4dd040d4a3234e2411"
+}
+
+variable "domain" {
+  default = "ethanbalik.com"
+}
+
+resource "cloudflare_dns_record" "www" {
+  zone_id = var.zone_id
+  name    = "www"
+  value   = "74.208.123.48"
+  type    = "A"
+  proxied = true
 }

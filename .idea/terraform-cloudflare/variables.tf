3369104bb574fa38b3cf1343369817d06010ce6c
Access Point <ebalikalaba@my.stlcc.edu>
2025-06-18 23:13:43 -0500

    split variables into their own folder

---
+++ .idea/terraform-cloudflare/variables.tf
@@ -0,0 +1,15 @@
+variable "zone_id" {
+  default = "7464bce49e4134da1b7a148a027d0a13"
+}
+
+variable "account_id" {
+  default = "e9fe21cb5a614d4dd040d4a3234e2411"
+}
+
+variable "domain" {
+  default = "ethanbalik.com"
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
